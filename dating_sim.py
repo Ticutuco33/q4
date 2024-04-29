ec8404c37a740fb0bca282716734ed8458a75289
Ticutuco33 <166734210+Ticutuco33@users.noreply.github.com>
2024-04-29 12:04:17 -0400

    Update dating_sim.py

--- dating_sim.py
+++ dating_sim.py
@@ -1,13 +1,12 @@
-
 print("❤️Welcome to Enzo's dating simulator❤️")
 
 def choose_a_date():
-    
+    print("choose a date: kratos, goku, sonic, master chief, gojo, po, gru and doomslayer"):
     
     
     
 def kratos():
-    
+    print("I am the god of war!! Human what do you want with me?")
 def goku():
     
 def sonic():
