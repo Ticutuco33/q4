2836bc89dc7d392e27947c65a3acc575030cd327
Ticutuco33 <166734210+Ticutuco33@users.noreply.github.com>
2024-04-29 11:27:25 -0400

    Update dating_sim.py

--- dating_sim.py
+++ dating_sim.py
@@ -1,2 +1,27 @@
 
-print("hey")
+print("❤️Welcome to Enzo's dating simulator❤️")
+
+def choose_a_date():
+    
+    
+    
+    
+def kratos():
+    
+def goku():
+    
+def sonic():
+    
+def dante():
+    
+def master_chief():
+    
+def gojo():
+    
+def po():
+    
+def gru():
+    
+def senator_armstrong():
+    
+def doom_guy():
