185d5edddc06f9d6c382c6cf69fe86ca9183613c
Ticutuco33 <166734210+Ticutuco33@users.noreply.github.com>
2024-04-30 12:06:51 -0400

    Update dating_sim.py

--- dating_sim.py
+++ dating_sim.py
@@ -1,26 +1,51 @@
 print("❤️Welcome to Enzo's dating simulator❤️")
 
 def choose_a_date():
-    print("choose a date: kratos, goku, sonic, master chief, gojo, po, gru and doomslayer"):
-    
+    date = input("Choose a date: Kratos, Goku, Sonic, Dante, Master Chief, Gojo, Po, Gru, Senator Armstrong and Doom Guy? ")
+    if date == "Kratos" or "kratos":
+        kratos()
+    elif date == "Goku" or "goku":
+        goku()
+    elif date == "Sonic" or "sonic":
+        sonic()
+    elif date == "Dante" or "dante":
+        kratos()
+    elif date == "Master Chief" or "master chief":
+        master_chief()
+    elif date == "Gojo" or "gojo":
+        gojo()
+    elif date == "Po" or "po":
+        po()
+    elif date == "Gru" or "gru":
+        gru()
+    elif date == "Senator Armstrong" or "senator armstrong":
+        senator_armstrong()
+    elif date == "Doom Guy" or "doom guy":
+        doom_guy()
+    else:
+        print("Please choose a available date")
     
     
 def kratos():
-    print("I am the god of war!! Human what do you want with me?")
+    print("I am the God of War!!! What do you want with me human?")
+    
 def goku():
-    
+    print("Hey, I am Goku, are you strong?")
 def sonic():
-    
+    print("Sup, I am Sonic, nice to meet, you do you want to race?")
 def dante():
-    
+    print("Hey Devil May Cry, I am Dante, would you like to kill some demons?")
 def master_chief():
-    
+    print("")
 def gojo():
-    
+    print("Your weak. I am Gojo the strongest sorcerer alive, would you like my help in killing curses?")
 def po():
-    
+    print("Hello I am Po, the Dragon Warrior, do you want to help me in saving China?")
 def gru():
-    
+    print("I am Gru, do you want to rob the moon with me?")
 def senator_armstrong():
-    
+    print("I am Senator Armstrong, do you like nano machines son?")
 def doom_guy():
+    print("Kill Demons?")
+    
+choose_a_date()
